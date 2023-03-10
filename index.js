4abd1c285ccd3ae84de2f9122c17cd819204f53a
efdalsahin <muhammetefdalsahin@gmail.com>
2023-03-10 18:59:06 +0300

    adding delete button

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     inputEl.value = "";
